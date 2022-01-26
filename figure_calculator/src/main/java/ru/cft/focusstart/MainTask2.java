3ef60fe494057e6665d3a68fad39b98c34ed0314
AshIgorWrld <ashigorwrld@gmail.com>
2022-01-26 23:36:51 +0700

    figure_calculator add

---
+++ figure_calculator/src/main/java/ru/cft/focusstart/MainTask2.java
@@ -0,0 +1,34 @@
+package ru.cft.focusstart;
+
+import lombok.extern.slf4j.Slf4j;
+import ru.cft.focusstart.parsers.InputParser;
+
+import java.io.File;
+
+@Slf4j
+public class MainTask2 {
+
+    public static void main(String[] args) {
+
+        if (args.length != 2) {
+            log.error("wrong arguments number");
+            return;
+        }
+        File inputFile = new File(args[0]);
+        InputParser inputParser = new InputParser();
+        String figureInfo = inputParser.takeInfo(inputFile);
+        if (null == figureInfo) {
+            return;
+        }
+        boolean isFileOutput;
+        if ("file".equals(args[1])) {
+            isFileOutput = true;
+        } else if ("cons".equals(args[1])) {
+            isFileOutput = false;
+        } else {
+            return;
+        }
+        FigureFabric figureFabric = new FigureFabric(isFileOutput);
+        figureFabric.takeFigure(figureInfo);
+    }
+}
